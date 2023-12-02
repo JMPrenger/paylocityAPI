b4b4bf92914af2bb3a6f30ffe181c7d2f668f088
Jeremy Prenger <37337236+JMPrenger@users.noreply.github.com>
2023-12-02 15:56:55 -0600

    added attributes to model

--- PaylocityAPI/PaylocityDto.cs
+++ PaylocityAPI/PaylocityDto.cs
@@ -1,9 +1,16 @@
-﻿namespace PaylocityAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaylocityAPI
 {
     public class PaylocityDto
     {
+        [StringLength(30)]
         public string name { get; set; }
+
+        [StringLength(30)]
         public string type { get; set; }
+
+        [StringLength(50)]
         public string description { get; set; }
         public DateOnly date { get; set; }
     }
